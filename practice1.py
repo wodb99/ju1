20af54673595b2f5bf43ddd6ae5c6cf004d9bcd0
김재유 <rlawodb2405@naver.com>
2025-01-24 17:49:28 +0900

    1

---
+++ practice1.py
@@ -0,0 +1,87 @@
+#print('hello')
+#print(1,2)
+#print('a','b')
+
+#print('김재유')
+#print(25)
+
+#print('김재유',25)
+
+#print('김재유', end=' ')
+#print(25)
+
+#print('###', end = ' ')
+#print('###')
+#print('#7#', end = ' ')
+#print('#3#')
+#print('###', end = ' ')
+#print('###')
+
+
+#print('안녕 나는 \'컴미\'야')
+
+# name = '김재유'
+# age = 25
+# print(f'저는 {name}이고, {age}세입니다.')
+
+# PI = 3.141592
+# print(f'{PI:.2f}')
+# print(f'{PI:.4f}')
+# print(int(PI))
+
+# a = 10.25
+# b = 20.31
+# sum1 = a + b
+# #print(sum1)
+# print(f'a와 b를 합치면 {sum1:.1f}이 됩니다.')
+
+# a = 10
+# print(f'10은 {id(a)}번방에 저장되어있다.')
+
+# a = 50
+# print(f'50은 {id(a)}번방에 저장되어있다.')
+
+#print(int('3.14'))
+
+# list1 = []
+# d = 3 + 3j
+# f = None
+
+
+# print(type('김재유'))
+# print(type(25))
+# print(type(3.14))
+# print(type(True))
+# print(type(list1))
+# print(type(d))
+# print(type(f))
+
+# a = 10
+# # a = a + 4
+# a += 4
+# print(a)
+
+# d = 11
+# d //= 2
+# print(d)
+
+# n = input()
+# print(n)
+
+# num = int(input())
+# print(num)
+
+# a, b = map(int, input().split())
+# print(a,b)
+
+# arr = list(map(int, input().split()))
+# print(arr)
+
+# a = list(map(int, input().split()))  # a = [map(int, input().split())]도 가능능
+# print(a)
+
+# b, c, d, e, f = map(int, input().split())
+# print(b, c, d, e, f)
+
+string = 'hello'
+string[start:end:step]
